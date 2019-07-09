5c5594a62c959e287d62738e8bb246b31edf4290
Ivanna <2015ivanka1997@gmail.com>
2019-07-10 00:05:55 +0300

    Add post to  form.
    Almost done task. There is trouble with reloading page.

--- aside/mock-server/server.js
+++ aside/mock-server/server.js
@@ -8,6 +8,7 @@ server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
     if (req.method === 'POST') {
         req.body.createdAt = Date.now();
+
     }
 
     next();
--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
 let selectCountry = document.getElementById("formControlSelect1");
+let adress = document.getElementById("address");
+let about_me = document.getElementById("about_me");
 let selectState = document.getElementById("formControlSelect2");
 let selectCity = document.getElementById("formControlSelect3");
 let URL = 'http://192.168.1.188:3000/';
 let usersList = document.getElementById("usersList");
+let form = document.getElementById('form');
+let submitBtn = document.getElementById('submitBtn');
 let country_Id;
 let state_Id;
 let State;
+let place;
+let CurentCity;
+let CurentState;
+let CurentCounrty;
+adress.defaultValue = null;
+about_me.defaultValue = null;
+document.addEventListener("DOMContentLoaded", getUsersList(`${URL}users`, usersList));
 
-// let StatesArr;
-
-
-// let CitiesArr;
-document.addEventListener("DOMContentLoaded", getUsersList(URL + 'users', usersList));
-// document.addEventListener("DOMContentLoaded", getStates(url + 'states'));
-// document.addEventListener("DOMContentLoaded", getCities(url + 'cities'));
-
-
-
-// async function getCountries(url) {
-//     let response = await fetch(url);
-//     CountriesArr = await response.json();
-//     return CountriesArr;
-// }
-
-// async function getStates(url) {
-//     let response = await fetch(url);
-//     StatesArr = await response.json();
-//     return StatesArr;
-// }
-// async function getCities(url) {
-//     let response = await fetch(url);
-//     CitiesArr = await response.json();
-//     return CitiesArr;
-// }
-
-document.addEventListener("DOMContentLoaded", getCountry(URL + 'countries', selectCountry));
+document.addEventListener("DOMContentLoaded", getCountry(`${URL}countries`, selectCountry));
 async function getCountry(url, select) {
     let response = await fetch(url);
     data = await response.json();
@@ -42,6 +26,7 @@ async function getCountry(url, select) {
         let option = document.createElement('option');
         option.innerText = item.name;
         option.dataset.id = item.id;
+        option.value = item.id;
         select.appendChild(option);
     });
 
@@ -54,8 +39,8 @@ selectCountry.onchange = function getCountryId() {
     let indexSelected = selectCountry.selectedIndex;
     let selectedOption = selectCountry.querySelectorAll('option')[indexSelected];
     country_Id = selectedOption.getAttribute('data-id');
-    getState(URL + 'states', selectState, country_Id);
-    console.log(country_Id + 'counrty iD');
+    getState(`${URL}states`, selectState, country_Id);
+
     return country_Id;
 }
 
@@ -67,6 +52,7 @@ async function getState(url, select, country_Id) {
         if (item.country_id == country_Id) {
             option.innerText = item.name;
             option.dataset.id = item.id;
+            option.value = item.id;
             select.appendChild(option);
         }
 
@@ -79,9 +65,7 @@ selectState.onchange = function getStateId() {
     let indexSelected = selectState.selectedIndex;
     State = selectState.querySelectorAll('option')[indexSelected];
     state_Id = State.getAttribute('data-id');
-    getCity(URL + 'cities', selectCity, state_Id);
-    console.log("Its  state id" + state_Id);
-    console.log(State);
+    getCity(`${URL}cities`, selectCity, state_Id);
     return state_Id;
 
 }
@@ -95,19 +79,44 @@ async function getCity(url, select, state_Id) {
         if (item.id == state_Id) {
             option.innerText = item.name;
             option.dataset.id = item.id;
+            option.value = item.id;
             select.appendChild(option);
         }
     });
     return data;
 }
 ///
+function GetStateById(url) {
+    fetch(url)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+            CurentState = data.name;
 
-////
+
+        })
+    return CurentState;
+}
+
+function GetCountryById(url) {
+    fetch(url)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+            CurentCounrty = data.name;
+
+
+        })
+    return CurentCounrty;
+}
+
 function refreshData(select) {
     for (var i = select.length; i >= 1; i--)
         select.remove(i);
 }
-let city;
+
 
 function getUsersList(url, list) {
     fetch(url)
@@ -116,50 +125,74 @@ function getUsersList(url, list) {
         })
         .then(function(data) {
             data.forEach(item => {
-
-                console.log(item.city_id);
-                fetch('http://192.168.1.188:3000/cities/' + item.city_id)
+                GetStateById(`http://192.168.1.188:3000/states/${item.state_id}`);
+                GetCountryById(`http://192.168.1.188:3000/countries/${item.country_id}`);
+                fetch(`http://192.168.1.188:3000/cities/${item.city_id}`)
                     .then(function(res) {
                         return res.json();
                     })
                     .then(function(data) {
-                        console.log(data.name);
-                       city = ` <p class="number">${data.name}</p>`;
-                       let person = document.createElement('div');
-                person.classList.add('person');
-                let info = `
+                        CurentCity = data.name;
+                        let date = (new Date(item.createdAt)).toLocaleString();
+                        let person = document.createElement('div');
+                        person.classList.add('person');
+                        let info = `
                <h2>
                    ${item.name}
                </h2>
                <p class="mail">${item.email}</p>
-               <p class="number">${item.phone_number}</p>
-               <p class="number">${city}</p>
-             
+               <p class="phone">${item.phone_number}</p>
+               <p class="city">${CurentCity}, ${CurentState}, ${CurentCounrty}</p>
+               <p class="date">${date}</p>
                `;
-                person.innerHTML = info;
-              
-                list.appendChild(person);
-          
-            }).catch(function(err) {
+                        person.innerHTML = info;
+
+                        list.appendChild(person);
+
+                    }).catch(function(err) {
                         console.log(err);
                     });
-             
-                
-        
-    })
-});
+
+
+
+            })
+        });
 }
 
-
+// submitBtn.onclick = function PostData() {
+//     let indexSelected = selectState.selectedIndex;
+//     State = selectState.querySelectorAll('option')[indexSelected];
+//     console.log("State", State.getAttribute("data-id"));
+//     let indexSelected1 = selectCountry.selectedIndex;
+//     let selectedOption = selectCountry.querySelectorAll('option')[indexSelected1];
+//     console.log("COUNTRY", selectedOption.getAttribute("data-id"));
+//     let indexSelected2 = selectCity.selectedIndex;
+//     let selectedOption2 = selectCity.querySelectorAll('option')[indexSelected2];
+//     console.log("city", selectedOption2.getAttribute("data-id"));
 // fetch(URL + 'users', {
-//     method: 'POST',
+//     method: 'post',
 //     headers: {
 //         'Content-Type': 'application/json'
 //     },
-//     body: JSON.stringify({
+//     body: formData
 
-//         name: "John",
-//         email: "john@example.com"
+// })
 
+
+
+
+
+
+
+
+// fetch(URL+'/users',{
+//     method:'post',
+//     body:formData
+// }).then(function(response){
+//             return response.text();
+//         }).then(function(text){
+//             console.log(text);
+//         }).catch(function(error){
+//     
 //     })
-// });
+// })
